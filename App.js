9906115d62eb46bf31c5358139ab0998c8f08d36
rafaelrabelodasilva <rafaelrabelodasilva@outlook.com>
2024-12-08 16:29:02 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { 
+  View, 
+  Text, 
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  FlatList
+} from "react-native";
 
-export default function App() {
+import Tarefa from "./src/Tarefa";
+
+import { FontAwesome } from '@expo/vector-icons'
+
+export default function App(){
+
+  const [tarefa, setTarefa] = useState('')
+  const [list, setList] = useState([])
+
+  function handleAdd() {
+    
+    if(tarefa === '') {
+      return
+    }
+
+    const dados = {
+      key: Date.now(),
+      item: tarefa
+    }
+
+    setList(oldArray => [dados, ...oldArray])
+
+    setTarefa('')
+  }
+
+  function handleDelete(item) {
+    let filtroItem = list.filter((tarefa) => {
+      return (tarefa.item !== item);
+    });
+  
+    setList(filtroItem);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tarefas</Text>
+      <View style={styles.containerInput}>
+        <TextInput 
+          placeholder="Digite sua tarefa..."
+          style={styles.input}
+          value={tarefa}
+          onChangeText={ (text) => setTarefa(text) }
+        />
+
+        <TouchableOpacity 
+          style={styles.buttonAdd}
+          onPress={handleAdd}
+          >
+          <FontAwesome name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList 
+      data={list}
+      keyExtractor={ (item) => item.key }
+      renderItem={ 
+        ({ item }) => <Tarefa data={item} deleteItem={ () => handleDelete(item.item)} />
+      }
+      style={styles.list}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2E4F4F',
+    paddingTop: 28,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#FFF',
+    marginTop: '5%',
+    paddingStart: '5%',
+    marginBottom: 12
+  },
+  containerInput: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 44,
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 22
   },
-});
+  input: {
+    width: '75%',
+    backgroundColor: '#FBFBFB',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8
+  },
+  buttonAdd: {
+    width: '15%',
+    height: 44,
+    backgroundColor: '#0E8388',
+    marginLeft: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4
+  },
+  list: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    paddingStart: '4%',
+    paddingEnd: '4%'
+  }
+})
---
+++ README.md
@@ -0,0 +1,67 @@
+# O Projeto ⭐
+
+Este projeto é uma aplicação de tarefas, onde o usuário pode adicionar novas tarefas e removê-las quando concluídas. Foi utilizado o **Expo** para simplificar o desenvolvimento do aplicativo em **React Native**. O Expo permite rodar o projeto tanto no desktop quanto no smartphone (quando conectados à mesma rede). Além disso, possibilita a emulação de um smartphone para acompanhar o desenvolvimento em tempo real.
+
+---
+
+## Ferramentas Utilizadas
+
+- **Expo**: Framework para desenvolvimento em React Native.
+  - Site oficial: [Aqui](https://expo.dev/)
+- **Android Studio**: Utilizado para emular dispositivos Android no desktop.
+  - Site oficial: [Aqui](https://developer.android.com/studio)
+
+---
+
+## Instalação do CLI do Expo
+
+Para instalar o CLI do Expo de forma global, execute o comando:
+
+```bash
+npm install --global expo-cli
+```
+
+Para verificar se a instalação foi realizada corretamente, confira a versão do Expo com o comando:
+
+```bash
+expo --version
+```
+
+---
+
+## Criando o Projeto
+
+Para iniciar um novo projeto com o Expo, utilize o comando:
+
+```bash
+expo init nomeDoProjeto
+```
+
+No meu caso, escolhi o template blank, que cria um projeto padrão com o mínimo necessário para iniciar o desenvolvimento.
+
+---
+
+## Executando o Projeto
+
+No diretório do projeto, execute o comando abaixo para iniciar o ambiente de desenvolvimento. Esse comando exibirá opções e gerará um QR Code para testar o app no smartphone:
+
+```bash
+npx expo start
+```
+
+---
+
+## Testando no Smartphone
+
+1. Baixe o aplicativo "Expo Go" na loja de aplicativos do seu smartphone.
+2. Após rodar o comando `npx expo start`, abra a câmera do smartphone e escaneie o QR Code gerado.
+3. O aplicativo será executado diretamente no smartphone, permitindo acompanhar o desenvolvimento em tempo real.
+
+---
+
+## Emulando no Desktop
+
+Para emular o aplicativo no desktop, utilize o Android Studio:
+
+* Baixe e instale o Android Studio: [Clique aqui](https://developer.android.com/studio)
+* Configure o emulador de dispositivos Android no Android Studio e conecte-o ao ambiente Expo.
